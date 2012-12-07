e39e4f9dcd63925a8bdbeaa3fbfd35206fe2d637
Sergii Kliuchnyk <sergiikliuchnyk@gmail.com>
2012-12-07 16:19:41 +0200

    createFineUploader plugin

---
+++ client/js/jquery.createfineuploader.plugin.js
@@ -0,0 +1,20 @@
+(function($){
+    $.fn.createFineUploader = function(ops){
+        var reg = />[\r\n\s]+</g, tpl, list;
+        if( list = this.find('.qq-upload-list') && list.length ) {
+            tpl = list.html().replace(reg, '><');
+            ops.fileTemplate = $.trim(tpl);
+            list.empty();
+        }
+
+        if( this.hasClass('qq-uploader') || this.find('.qq-uploader').length ) {
+            tpl = this.html().replace(reg, '><');
+            ops.template = $.trim(tpl);
+            this.empty();
+        }
+
+        ops.element = this.get(0);
+
+        return new qq.FineUploader(ops);
+    };
+})(jQuery);
